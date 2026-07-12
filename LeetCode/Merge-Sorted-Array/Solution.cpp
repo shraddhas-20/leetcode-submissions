69b849ee8c0bb76ae90f764ef4599bf6876c8fa4
Shraddha Singh <shraddhaforcode@gmail.com>
2026-07-12 20:57:13 +0530

    Add solution for Merge Sorted Array

--- LeetCode/Merge-Sorted-Array/Solution.cpp
+++ LeetCode/Merge-Sorted-Array/Solution.cpp
@@ -1,25 +1,65 @@
-1class Solution {
-2public:
-3    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-4        int i = m - 1;      
-5        int j = n - 1;      
-6        int k = m + n - 1;  
-7
-8        while (i >= 0 && j >= 0) {
-9            if (nums1[i] > nums2[j]) {
-10                nums1[k] = nums1[i];
-11                i--;
-12            } else {
-13                nums1[k] = nums2[j];
-14                j--;
-15            }
-16            k--;
-17        }
-18
-19        while (j >= 0) {
-20            nums1[k] = nums2[j];
-21            j--;
-22            k--;
-23        }
-24    }
-25};
+/*
+
+    Time Complexity : O(MlogM), Sorting the array(nums1) costs O(MlogM). Where M is the size of the
+    Array(nums1).
+
+    Space Complexity : O(1), Constant Space. 
+
+    Solved using Array + Sorting.
+
+*/
+
+
+/***************************************** Approach 1 *****************************************/
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i=0; i<n; i++){
+            nums1[m+i] = nums2[i];
+        }
+        sort(nums1.begin(), nums1.end());
+    }
+};
+
+
+
+
+
+
+/*
+
+    Time Complexity : O(N+M), Because we iterate N+M times for both the Array(nums1 and nums2). Where
+    M is the number of elements in Array(nums1) and N is the number of elements in Array(nums2).
+
+    Space Complexity : O(1), Constant Space. 
+
+    Solved using Array + Two Pointers. 
+
+*/
+
+
+/***************************************** Approach 2 *****************************************/
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                i--; k--;
+            }
+            else{
+                nums1[k] = nums2[j];
+                j--; k--;
+            }
+        }
+        while(j>=0){
+            nums1[k] = nums2[j];
+            j--; k--;
+        }
+    }
+};
